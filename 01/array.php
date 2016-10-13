f9161a6ae90f92ff29e06de048ab2b53f3621113
sashalemesh <lemesh37@gmail.com>
2016-10-13 10:14:12 +0300

    new array1

---
+++ 01/array.php
@@ -0,0 +1,53 @@
+﻿<?php
+/*
+ЗАДАНИЕ 1
+- Создайте массив $bmw с ячейками:
+	"model"
+	"speed"
+	"doors"
+	"year"
+- Заполните ячейки значениями: "X5", 120, 5, "2006"	
+- Создайте массивы $toyota и $opel аналогичные массиву $bmw.
+- Заполните массив $toyota значениями: "Carina", 130, 4, "2007"
+- Заполните массив $opel значениями: "Corsa", 140, 5, "2007"
+*/
+	$bmw = array(
+		"model"=> "X5",
+		"speed"=> 120,
+		"doors"=> 5,
+		"year"=> "2006"
+	);	
+	$toyota = array(
+		"model"=> "Carina",
+		"speed"=> 130,
+		"doors"=> 4,
+		"year"=> "2007"
+	);
+	$opel = array(
+		"model"=> "Corsa",
+		"speed"=> 140,
+		"doors"=> 5,
+		"year"=> "2007"
+	);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<head>
+	<title>Массивы</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+</head>
+<body>
+	<h1>Массивы</h1>
+	<?php
+	/*
+	ЗАДАНИЕ 2
+	- Выведите значения всех трёх массивов в виде: name - model - speed - doors -year,  например: bmw - 120 - 5 - 2006
+	*/
+	
+	echo "BMW - $bmw[model] - $bmw[speed] - $bmw[doors] - $bmw[year] <br>";
+	echo "toyota - {$toyota["model"]} - {$toyota["speed"]} - {$toyota["doors"]} - {$toyota["year"]} <br>";//Экранируем переменные{}
+	echo "opel - $opel[model] - $opel[speed] - $opel[doors] - $opel[year] <br>";
+	?>
+</body>
+</html>
---
+++ 01/array1.php
@@ -0,0 +1,85 @@
+﻿<?php
+
+$str = "Hello";
+echo $str{0}."<br>";//H
+echo $str[0]."<br>";//H
+
+
+echo "array Массивы <br>";//array Массивы
+	$user[11]="John";
+	$user[]="root";
+	$user[44]="1234";
+	$user[]=25;
+	$user[]=true;
+	
+//Тоже самое
+	$user1=array(
+		0=>"John",
+		"root",
+		17=>"1234",
+		25,
+		true
+	);	
+	
+	echo '<pre>';
+		print_r($user);//Печатает информацию об объекте
+	echo '</pre>';
+	
+		echo '<pre>';
+			var_dump($user1);//Печатает информацию об объекте (расширенную)
+		echo '</pre>';
+	//echo count($user);//Выводит длину масива - 5
+	echo '<hr>';
+	
+	//array Ассоциативные Массивы
+	$user2[name]="John";
+	$user2[login]="root";
+	$user2[pass]="1234";
+	$user2[age]=25;
+	$user2[]=true;
+	
+		//Тоже самое
+	$user3=array(
+		name=>"John",
+		login=>"root",
+		pass=>"1234",
+		age=>25,
+		true
+	);	
+	
+	echo '<pre>';
+		print_r($user2);//Печатает информацию об объекте
+	echo '</pre>';
+	
+		echo '<pre>';
+			var_dump($user3);//Печатает информацию об объекте (расширенную)
+		echo '</pre>';
+		echo "<hr>";
+		
+		echo "Многомерные массивы <br>";//Многомерные массивы
+	$cars["bmw"] = array(
+		"model"=> "X5",
+		"speed"=> 120,
+		"doors"=> 5,
+		"year"=> "2006"
+	);	
+	$cars["toyota"] = array(
+		"model"=> "Carina",
+		"speed"=> 130,
+		"doors"=> 4,
+		"year"=> "2007"
+	);
+	$cars["opel"] = array(
+		"model"=> "Corsa",
+		"speed"=> 140,
+		"doors"=> 5,
+		"year"=> "2007"
+	);
+	echo $cars["opel"][speed].'<br>';
+	echo '<hr>';
+	echo "Konstant <br>";//Konstant
+	define('AAA',100);
+	if(!defined('AAA'))
+		define('AAA',200);
+	echo AAA;
+?>
